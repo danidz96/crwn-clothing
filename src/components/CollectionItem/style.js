8e08c988e9dc6191ceb9a3a7dc80e0cad5b18641
Dani Domínguez <dani.d.zamorano@gmail.com>
2020-03-23 20:28:31 +0100

    rename styled container

--- src/components/CollectionItem/style.js
+++ src/components/CollectionItem/style.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const Wrapper = styled.div`
+export const CollectionItemContainer = styled.div`
   width: 22%;
   display: flex;
   flex-direction: column;
