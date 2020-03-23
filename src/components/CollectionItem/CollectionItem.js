c6f63b22c80d4bf9d6a25e5013f3ee35781e399f
Dani Domínguez <dani.d.zamorano@gmail.com>
2020-03-23 20:28:18 +0100

    rename sytyled container

--- src/components/CollectionItem/CollectionItem.js
+++ src/components/CollectionItem/CollectionItem.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
-import { Wrapper, Image, Footer, Name, Price } from './style';
+import { CollectionItemContainer, Image, Footer, Name, Price } from './style';
 
 const CollectionItem = ({ id, name, price, imageUrl }) => {
   return (
-    <Wrapper>
+    <CollectionItemContainer>
       <Image imageUrl={imageUrl} />
       <Footer>
         <Name>{name}</Name>
         <Price>{price}</Price>
       </Footer>
-    </Wrapper>
+    </CollectionItemContainer>
   );
 };
 
