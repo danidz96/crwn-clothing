58e32689d7f1d3aecafcd9c26c9a2005077c562b
Dani Domínguez <dani.d.zamorano@gmail.com>
2020-03-28 12:48:07 +0100

    save user logged with google

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
 import ShopPage from './pages/Shop/ShopPage';
 import Header from './components/Header/Header';
 import GlobalStyles from './GlobalStyles';
 import SignInAndSignUp from './pages/SignInAndSignUp/SignInAndSignUp';
+import { auth } from './firebase/utils/firebase';
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      setCurrentUser(user);
+    });
+
+    return () => {
+      unsubscribeFromAuth();
+    };
+  }, []);
+
   return (
     <>
       <Header />
