fc12fc4035de3ef87381fe52155e48844583c758
Dani Domínguez <dani.d.zamorano@gmail.com>
2020-04-10 12:15:07 +0200

    create cart item

---
+++ src/components/CartItem/CartItem.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+  CartItemContainer,
+  ItemDetailsContainer,
+  CartItemImage,
+} from './style';
+
+const CartItem = ({ item: { imageUrl, price, name, quantity } }) => {
+  return (
+    <CartItemContainer>
+      <CartItemImage src={imageUrl} alt="item" />
+      <ItemDetailsContainer>
+        <span>{name}</span>
+        <span>
+          {quantity} x ${price}
+        </span>
+      </ItemDetailsContainer>
+    </CartItemContainer>
+  );
+};
+
+export default CartItem;
