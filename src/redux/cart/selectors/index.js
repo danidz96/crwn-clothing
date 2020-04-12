136448ac2b5b38d467eae9268aca0412eafe1c0d
Dani Domínguez <dani.d.zamorano@gmail.com>
2020-04-12 13:25:07 +0200

    add selectCartTotal selector

--- src/redux/cart/selectors/index.js
+++ src/redux/cart/selectors/index.js
@@ -21,3 +21,10 @@ export const selectCartItemsCount = createSelector(
       0
     )
 );
+export const selectCartTotal = createSelector([selectCartItems], cartItems =>
+  cartItems.reduce(
+    (accumalatedQuantity, cartItem) =>
+      accumalatedQuantity + cartItem.quantity * cartItem.price,
+    0
+  )
+);
