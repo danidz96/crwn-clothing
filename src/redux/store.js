515545b44ca55a7cb5b67c59d1218b207de69ea8
Dani Domínguez <dani.d.zamorano@gmail.com>
2020-04-13 16:37:25 +0200

    setup redux store with redux-persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,34 @@
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
-import userSlice from './user/userSlice';
-import cartSlice from './cart/cartSlice';
+import {
+  configureStore,
+  getDefaultMiddleware,
+  combineReducers,
+} from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import userReducer from './user/userSlice';
+import cartReducer from './cart/cartSlice';
 
-export default configureStore({
-  reducer: { user: userSlice, cart: cartSlice },
+const reducers = combineReducers({
+  user: userReducer,
+  cart: cartReducer,
+});
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart'],
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+const store = configureStore({
+  reducer: persistedReducer,
   middleware: getDefaultMiddleware({
     serializableCheck: false,
     immutableCheck: false,
   }),
 });
+
+export const persistor = persistStore(store);
+
+export default store;
