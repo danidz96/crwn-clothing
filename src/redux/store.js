3e1c2b084c016b8a1843b6f1733a3bdadc168e70
Dani Domínguez <dani.d.zamorano@gmail.com>
2020-04-17 17:55:36 +0200

    move directory sections

--- src/redux/store.js
+++ src/redux/store.js
@@ -7,10 +7,12 @@ import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import userReducer from './user/userSlice';
 import cartReducer from './cart/cartSlice';
+import directoryReducer from './directory/directorySlice';
 
 const reducers = combineReducers({
   user: userReducer,
   cart: cartReducer,
+  directory: directoryReducer,
 });
 
 const persistConfig = {
