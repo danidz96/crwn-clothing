dc67f7acb59afc6895fcdb37ad76f45f9d06c87b
Dani Domínguez <dani.d.zamorano@gmail.com>
2020-04-19 17:43:31 +0200

    setup collection routing

--- src/pages/Shop/ShopPage.js
+++ src/pages/Shop/ShopPage.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import CollectionsOverview from '../../components/CollectionsOverview/CollectionsOverview';
+import CollectionPage from '../Collection/CollectionPage';
 
-const ShopPage = () => {
-  return <CollectionsOverview />;
+const ShopPage = ({ match }) => {
+  return (
+    <>
+      <Route exact path="/shop">
+        <CollectionsOverview />
+      </Route>
+      <Route path="/shop/:collectionId">
+        <CollectionPage />
+      </Route>
+    </>
+  );
 };
 
 export default ShopPage;
